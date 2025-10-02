4fc72effc37039d9fc58f050316c628e43b27e1d
Abdur Rahman <19cse040@gmail.com>
2025-10-03 01:20:24 +0600

    add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,36 +2,25 @@ module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
   parserOptions: {
+    project: "./tsconfig.json",
     ecmaVersion: 2021,
     sourceType: "module",
-    project: "./tsconfig.json",
   },
   env: {
     node: true,
     es2021: true,
-    jest: true, // if using Jest
+    jest: true,
   },
   plugins: ["@typescript-eslint", "prettier"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended", // enables eslint-plugin-prettier + displays prettier errors
+    "plugin:prettier/recommended"
   ],
   rules: {
-    // Core best practices
     "no-console": "warn",
-    "eqeqeq": ["error", "always"],
-    "curly": ["error", "all"],
-
-    // TypeScript rules
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }
-    ],
-    "@typescript-eslint/explicit-function-return-type": "off", // optional
-    "@typescript-eslint/no-explicit-any": "warn", // avoid too much "any"
-
-    // Formatting rules (handled by Prettier)
-    "prettier/prettier": "error",
-  },
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-explicit-any": "warn",
+    "prettier/prettier": "error"
+  }
 };
